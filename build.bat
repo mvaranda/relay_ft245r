87f28f96b184541759ad250d967c1b80e3a4496c
Marcelo Varanda <mv_email@yahoo.com>
2019-02-09 08:49:16 -0800

    add license

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 echo off
 
-rem Warning: if the build fails then you have to add the MinGW bin folder into the path, for example:
+rem    Warning: if the build fails then you have to add the MinGW bin folder into the path, for example:
+rem    if you have codeblocks installed then:
 rem          path=c:\Program Files (x86)\CodeBlocks\MinGW\bin;%path%
+rem    if you have mingw installed from mingw.org:
+rem          path=C:\MinGW\bin;%path%
+rem
+rem    Note: it seems that Mingw64 does not work even forcing to compile in 32bits due FTD2XX library linking.
+rem    MinGW is available for free at: https://osdn.net/projects/mingw/downloads/68260/mingw-get-setup.exe/
+rem          
 
 mingw32-gcc.exe -Wall -g  -c relay_ft245r.c -o relay_ft245r.o
 
--- relay_ft245r.c
+++ relay_ft245r.c
@@ -1,4 +1,28 @@
 
+/*
+
+MIT License
+
+Copyright (c) 2019 Marcelo Varanda
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +30,9 @@
 #include "FTD2XX.H"
 
 #define USAGE \
-"Relay FT245R - ver 0.01\n\
+"\nRelay FT245R - ver 0.01\n\
+source: https://github.com/mvaranda/relay_ft245r\n\
+Copyright (c) 2019 Marcelo Varanda - MIT License\n\n\
 Menu:\n\
    r N on|off    - where N is the relay number (1~8) or \'all\'\n\
    list          - show all FTDI devices\n\
